82433f915cd3ccad21e9b29e5a174847f13da6e7
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2022-08-01 03:28:10 -0400

    To deploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      body: "Bienvenidos! Welcome! Benvenue!",
+      from: "InteliBot",
+    },
+  ]);
+
+  useEffect(() => {
+    const receiveMessage = (message) => {
+      setMessages([message, ...messages]);
+      console.log(message);
+    };
+
+    //Suscribe.
+    socket.on("message", receiveMessage);
+
+    //UnSuscribe.
+    return () => socket.off("message", receiveMessage);
+  }, [messages]);
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+    socket.emit("message", message);
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages([newMessage, ...messages]);
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10 rounded-sm">
+        <h1 className="text-2xl font-bold my-2">Simple React Chat</h1>
+        <input
+          className="border-2 border-zinc-500 text-black w-full"
+          type="text"
+          onChange={handleChange}
+          value={message}
+        />
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`p-2 my-2 table text-sm rounded-md bg-sky-700 ${
+                message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+              }`}
+            >
+              <p>
+                <span>From:{message.from}</span> - {message.body}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </form>
     </div>
   );
 }
